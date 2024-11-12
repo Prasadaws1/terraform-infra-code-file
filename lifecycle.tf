a27809c43f435ce17241320b421fbc0994c78a83
prasad <prasadyadav5906@gmail.com>
2024-11-12 16:02:20 +0530

    Create lifecycle.tf

---
+++ lifecycle.tf
@@ -0,0 +1,8 @@
+lifecycle {
+prevent_destroy = true ###############--------------------->>>>>>>>>>>>>>> this help cant destroy
+}
+
+
+lifecycle {
+ignore_changes = [tags, instance_type, instance_type] #################################--------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>> this can be help to ignore modification 
+}
